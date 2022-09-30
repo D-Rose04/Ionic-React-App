d49a7beca3c5b2f7dde45685c1cca56b8333b415
D-Rose04 <davidbhr04@gmail.com>
2022-09-29 22:51:27 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Tarea3',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
