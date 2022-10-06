d990ddfb74ae1cc9ae799f34bb853a0d450566b3
D-Rose04 <davidbhr04@gmail.com>
2022-10-05 22:06:21 -0400

    multiply finished

--- src/pages/Multiplication/Multiplication.tsx
+++ src/pages/Multiplication/Multiplication.tsx
@@ -1,10 +1,18 @@
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle } from '@ionic/react';
-import React from 'react';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonButton, IonContent, IonInput, IonItem, IonLabel } from '@ionic/react';
+import React, { useState } from 'react';
+import { TwoNumberOperation } from '../../interfaces/TwoNumberOperation';
 import './Multiplication.css';
 
 const Multiplication = () => {
+  const [Value, SetValue] = useState<TwoNumberOperation>({ a: '', b: '', result: 0 });
+
+  const Multiply = (a: number, b: number): number => {
+    return a * b;
+  }
+
   return (
-    <IonHeader>
+    <>
+      <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
             <IonMenuButton />
@@ -12,6 +20,21 @@ const Multiplication = () => {
           <IonTitle>Multiplication</IonTitle>
         </IonToolbar>
       </IonHeader>
+
+      <IonContent>
+        <IonItem>
+          <IonLabel position="floating">Enter a number:</IonLabel>
+          <IonInput id="a" required clearInput type='number' onIonChange={(e) => SetValue({ ...Value, a: e.target.value?.toString() })}></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Enter a number:</IonLabel>
+          <IonInput id="b" required clearInput type='number' onIonChange={(e) => SetValue({ ...Value, b: e.target.value?.toString() })}></IonInput>
+        </IonItem>
+        <IonButton expand="block" onClick={() => SetValue({ ...Value, result: Multiply(Number(Value.a), Number(Value.b)) })}>Submit</IonButton>
+        <IonLabel>{Value.result}</IonLabel>
+      </IonContent>
+
+    </>
   )
 }
 export default Multiplication;
