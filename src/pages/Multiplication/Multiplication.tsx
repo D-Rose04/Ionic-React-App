2968bfc66f9d65ca82cbe88caedacdc40a5b1414
D-Rose04 <davidbhr04@gmail.com>
2022-10-05 22:39:34 -0400

    Multiply finished

--- src/pages/Multiplication/Multiplication.tsx
+++ src/pages/Multiplication/Multiplication.tsx
@@ -1,13 +1,21 @@
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonButton, IonContent, IonInput, IonItem, IonLabel } from '@ionic/react';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonButton, IonContent, IonInput, IonItem, IonLabel, IonList } from '@ionic/react';
 import React, { useState } from 'react';
+import { TableOperation } from '../../interfaces/TableOpeartion';
 import { TwoNumberOperation } from '../../interfaces/TwoNumberOperation';
+
 import './Multiplication.css';
 
 const Multiplication = () => {
-  const [Value, SetValue] = useState<TwoNumberOperation>({ a: '', b: '', result: 0 });
+  const [Value, SetValue] = useState<TableOperation>({ result: [] });
+  const [number,SetNumber] = useState<number>(0);
 
-  const Multiply = (a: number, b: number): number => {
-    return a * b;
+  const Multiply = (value: number, times: number): Array<Number> => {
+    SetValue({ ...Value, result: [] });
+    let results = [];
+    for (let i = 1; i < times + 1; i++) {
+      results.push(i  * value)
+    }
+    return results;
   }
 
   return (
@@ -24,14 +32,18 @@ const Multiplication = () => {
       <IonContent>
         <IonItem>
           <IonLabel position="floating">Enter a number:</IonLabel>
-          <IonInput id="a" required clearInput type='number' onIonChange={(e) => SetValue({ ...Value, a: e.target.value?.toString() })}></IonInput>
+          <IonInput id="a" required clearInput type='number' onIonChange={(e) => SetNumber(Number(e.target.value?.toString()))}></IonInput>
         </IonItem>
-        <IonItem>
-          <IonLabel position="floating">Enter a number:</IonLabel>
-          <IonInput id="b" required clearInput type='number' onIonChange={(e) => SetValue({ ...Value, b: e.target.value?.toString() })}></IonInput>
-        </IonItem>
-        <IonButton expand="block" onClick={() => SetValue({ ...Value, result: Multiply(Number(Value.a), Number(Value.b)) })}>Submit</IonButton>
-        <IonLabel>{Value.result}</IonLabel>
+        <IonButton expand="block" onClick={() => SetValue({ ...Value, result: Multiply(number, 14) })}>Submit</IonButton>
+        <IonList>
+          {Value.result.map((result,index) => {
+            return (
+              <IonItem>
+                <IonLabel>{number} X {index + 1} = {result.toString()}</IonLabel>
+              </IonItem>
+            )
+          })}
+        </IonList>
       </IonContent>
 
     </>
