f44f8468f37bd5c1f636e7665ed761afc3e25105
D-Rose04 <davidbhr04@gmail.com>
2022-10-05 22:02:58 -0400

    addNumbers finished

---
+++ src/interfaces/TwoNumberOperation.ts
@@ -0,0 +1,5 @@
+export interface TwoNumberOperation {
+    a: string | undefined;
+    b: string | undefined;
+    result: number;
+}
--- src/pages/AddNumbers/AddNumber.tsx
+++ src/pages/AddNumbers/AddNumber.tsx
@@ -1,10 +1,18 @@
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle } from '@ionic/react';
-import React from 'react';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonInput, IonItem, IonLabel, IonButton, IonToast } from '@ionic/react';
+import React, { useState } from 'react';
+import { TwoNumberOperation } from '../../interfaces/TwoNumberOperation';
 import './AddNumber.css';
 
 const AddNumber: React.FC = () => {
-    return (
-        <IonHeader>
+  const [Value, SetValue] = useState<TwoNumberOperation>({ a: '', b: '', result: 0 });
+
+  const addNumbers = (a: number, b: number): number => {
+    return a + b;
+  }
+
+  return (
+    <>
+      <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
             <IonMenuButton />
@@ -12,8 +20,25 @@ const AddNumber: React.FC = () => {
           <IonTitle>Add Numbers</IonTitle>
         </IonToolbar>
       </IonHeader>
-    )
+      <IonContent>
+        <IonItem>
+          <IonLabel position="floating">Enter a number:</IonLabel>
+          <IonInput id="a" required clearInput type='number' onIonChange={(e) => SetValue({ ...Value, a: e.target.value?.toString() })}></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Enter a number:</IonLabel>
+          <IonInput id="b" required clearInput type='number' onIonChange={(e) => SetValue({ ...Value, b: e.target.value?.toString() })}></IonInput>
+        </IonItem>
+        <IonButton expand="block" onClick={() => SetValue({ ...Value, result: addNumbers(Number(Value.a), Number(Value.b)) })}>Submit</IonButton>
+        <IonLabel>{Value.result}</IonLabel>
+      </IonContent>
+    </>
+  )
 }
 
 export default AddNumber;
+
+function useIonToast(): [any] {
+  throw new Error('Function not implemented.');
+}
 
