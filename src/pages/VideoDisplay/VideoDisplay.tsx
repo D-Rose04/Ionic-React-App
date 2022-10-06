48855c11a045a62c54e73a92b1f82a7788c04e21
D-Rose04 <davidbhr04@gmail.com>
2022-10-05 22:47:01 -0400

    video display finished

--- src/pages/VideoDisplay/VideoDisplay.tsx
+++ src/pages/VideoDisplay/VideoDisplay.tsx
@@ -1,10 +1,11 @@
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle } from '@ionic/react';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent, IonLabel } from '@ionic/react';
 import React from 'react';
 import './videoDisplay.css';
 
 const VideoDisplay = () => {
-    return (
-        <IonHeader>
+  return (
+    <>
+      <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
             <IonMenuButton />
@@ -12,7 +13,15 @@ const VideoDisplay = () => {
           <IonTitle>Video</IonTitle>
         </IonToolbar>
       </IonHeader>
-    )
+
+      <IonContent>
+        <div className='videoContainer'>
+          <IonLabel>Mi experiencia con Ionic</IonLabel>
+          <iframe width="95%" height="30%" src="https://www.youtube.com/embed/-aHBhvkrFlY" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        </div>
+      </IonContent>
+    </>
+  )
 }
 
 export default VideoDisplay;
